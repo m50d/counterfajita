26cdeb7eed97f1b67909446348caf2727e2cf4a8
Michael Donaghy <md401@srcf.ucam.org>
2014-01-12 13:59:13 +0000

    vague idea

--- src/main/scala/net/homelinux/md401/counterfajita/CounterFajita.scala
+++ src/main/scala/net/homelinux/md401/counterfajita/CounterFajita.scala
@@ -3,10 +3,14 @@ package net.homelinux.md401.counterfajita
 import akka.actor.Actor
 import akka.actor.FSM
 
+sealed trait PersonState
+case object Unemployed extends PersonState
+case class Employed() extends PersonState
+
 object CounterFajita {
 	
 }
 
-class Person extends Actor with FSM[Any, Any] {
+class Person extends Actor with FSM[PersonState, Any] {
   
 }
